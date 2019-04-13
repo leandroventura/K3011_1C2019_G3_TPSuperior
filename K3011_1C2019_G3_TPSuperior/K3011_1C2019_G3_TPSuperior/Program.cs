92569b894ee2d9fbf8871580d5f5a6207b3ce75e
leandroventura <ventura.leandro@hotmail.com>
2019-04-13 00:34:18 -0300

    Primer commit!

---
+++ K3011_1C2019_G3_TPSuperior/K3011_1C2019_G3_TPSuperior/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace K3011_1C2019_G3_TPSuperior
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
